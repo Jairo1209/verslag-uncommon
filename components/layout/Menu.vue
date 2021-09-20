569e3c0fb7ac7932f5c7aae3890003429e01ac42
Jairo1209 <87414697+Jairo1209@users.noreply.github.com>
2021-09-20 10:30:38 +0200

    refactor menu css

--- components/layout/Menu.vue
+++ components/layout/Menu.vue
@@ -3,24 +3,24 @@
     class="menu"
     :class="{ 'is-open' : menuIsOpen }"
   >
-    <nav>
-      <ul>
-        <li>
-          <NuxtLink class="nav-link" to="/forest">
+    <nav class="menu__nav">
+      <ul class="list-unstyled menu__list">
+        <li class="menu__item">
+          <NuxtLink class="menu__link" to="/forest">
             Forest
-            <span />
+            <span class="menu__line" />
           </NuxtLink>
         </li>
-        <li>
-          <NuxtLink class="nav-link" to="/ocean">
+        <li class="menu__item">
+          <NuxtLink class="menu__link" to="/ocean">
             Ocean
-            <span />
+            <span class="menu__line" />
           </NuxtLink>
         </li>
-        <li>
-          <NuxtLink class="nav-link" to="/desert">
+        <li class="menu__item">
+          <NuxtLink class="menu__link" to="/desert">
             Desert
-            <span />
+            <span class="menu__line" />
           </NuxtLink>
         </li>
       </ul>
@@ -45,8 +45,6 @@ export default {
     menuIsOpen (isOpen) {
       if (isOpen) {
         this.playAnimation()
-      } else {
-        this.closeAnimation()
       }
     }
   },
@@ -60,25 +58,27 @@ export default {
 
   // 1x uitgevoerd als component wordt ingeladen
   mounted () {
-    this.animation = gsap.fromTo('.nav-link',
+    this.animation = gsap.timeline({
+      paused: true
+    })
+    this.animation.fromTo('.menu__item',
       {
-        opacity: 0
+        opacity: 0,
+        y: -30
       },
       {
+        y: 0,
         opacity: 1,
         duration: 1.2,
         stagger: 0.2,
-        paused: true
-      })
+        ease: 'power4.out'
+      }, 0.3)
   },
 
   // hier staan functies / methods in die je aan kunt roepen in je hele component
   methods: {
     playAnimation () {
-      this.animation.play()
-    },
-    closeAnimation () {
-      this.animation.reverse()
+      this.animation.restart()
     }
   }
 }
@@ -113,58 +113,54 @@ export default {
     visibility: visible;
     opacity: 1;
   }
-}
 
-nav {
-  width: 100%;
-  height: 100%;
+  &__nav {
+    width: 100%;
+    height: 100%;
+  }
 
-  ul {
+  &__list {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     width: 100%;
     height: 100%;
-    padding: 0;
-    margin: 0;
-    list-style-type: none;
   }
 
-  li {
+  &__item {
     position: relative;
     display: flex;
     align-items: center;
     justify-content: center;
     width: 100%;
     height: calc(100% / 6);
-    min-height: 100px;
-    font-family: 'Kaisei Tokumin', serif;
-    font-size: rem(50px);
-    font-style: normal;
-    font-weight: bold;
+    min-height: rem(100px);
+    text-transform: uppercase;
+
+    @include get-font-size(menu);
+    @include headingstyles();
   }
-}
 
-span {
-  position: absolute;
-  bottom: 0;
-  display: block;
-  width: 100%;
-  max-width: 180px;
-  height: 2px;
-  background-color: white;
-  opacity: 0;
-}
+  &__line {
+    position: absolute;
+    bottom: 0;
+    display: block;
+    width: 100%;
+    height: rem(2px);
+    background-color: theme-color(light);
+    opacity: 0;
+  }
 
-.nav-link {
-  position: relative;
-  display: block;
-  color: white;
-  cursor: pointer;
+  &__link {
+    position: relative;
+    display: block;
+    color: theme-color(light);
+    cursor: pointer;
 
-  &.nuxt-link-active span {
-    opacity: 1;
+    &.nuxt-link-active .menu__line {
+      opacity: 1;
+    }
   }
 }
 </style>
